4833feccd052620a1061dfa9a1851876fc084c57
ngocbau12324 <ngocbau2015tqk@gmail.com>
2022-05-04 14:34:32 +0700

    Daily Selfie App Android

--- app/src/main/java/com/example/bt4_dailyselfie/MainActivity.java
+++ app/src/main/java/com/example/bt4_dailyselfie/MainActivity.java
@@ -1,14 +1,74 @@
 package com.example.bt4_dailyselfie;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private String mCurrentSelfieName;
+    private String mCurrentPhotoPath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_camera:
+                openCameraIntent();
+                break;
+            case R.id.action_delete_selected:
+                break;
+            case R.id.action_delete_all:
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    private File createImageFile() throws IOException{
+        //Create an image file name
+        mCurrentSelfieName = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File imageFile = File.createTempFile(mCurrentSelfieName, ".jpg",getExternalFilesDir(null));
+        mCurrentPhotoPath = imageFile.getAbsolutePath();
+        return imageFile;
+    }
+    private void openCameraIntent(){
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(cameraIntent.resolveActivity(getPackageManager())!=null){
+            File photoFile = null;
+            try{
+                photoFile = createImageFile();
+            }catch (IOException e){
+                //
+            }
+            if(photoFile != null){
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
     }
 }
